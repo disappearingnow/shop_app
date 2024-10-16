e7e2caf42f76662fd59e1bcbb1b448f5795c724a
disappearingnow <disappearing.now@gmail.com>
2024-10-16 17:32:10 -0400

    add auth provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { AuthContextProvider } from "./providers/authProvider.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </StrictMode>
+);
