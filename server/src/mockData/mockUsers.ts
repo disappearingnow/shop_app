29232008a3a07f99441924b79cea32ff573f3c44
disappearingnow <disappearing.now@gmail.com>
2024-10-23 16:27:06 -0400

    mock data for api

---
+++ server/src/mockData/mockUsers.ts
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    id: 1,
+    name: "Karrie",
+    last_name: "Bonnick",
+    email: "kbonnick0@yahoo.com",
+    password: "KBonnick",
+  },
+  {
+    id: 2,
+    name: "Vinny",
+    last_name: "Bau",
+    email: "vbau1@gmail.com",
+    password: "VBau",
+  },
+  {
+    id: 3,
+    name: "Briana",
+    last_name: "Cardenosa",
+    email: "bcardenosa2@hotmail.com",
+    password: "BCardenosa",
+  },
+  {
+    id: 4,
+    name: "Cecilla",
+    last_name: "Behneke",
+    email: "cbehneke3@yahoo.com",
+    password: "CBehneke",
+  },
+  {
+    id: 5,
+    name: "Maia",
+    last_name: "Becerra",
+    email: "mbecerra4@gmail.com",
+    password: "MBecerra",
+  },
+];
