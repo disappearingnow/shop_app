3b6f4bb21f65c83f858c28945dd5342e0c821ad4
disappearingnow <disappearing.now@gmail.com>
2024-10-23 16:29:11 -0400

    add authRoutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,21 @@
 // Import the framework and instantiate it
 import Fastify from "fastify";
-import { productsRoutes } from "./routes/index.js";
+import { productsRoutes, authRoutes } from "./routes/_index.js";
 const server = Fastify();
 
 const PORT = 3000;
 
 server.register(productsRoutes);
+server.register(authRoutes);
 
-/* 
 server.get("/", async function handler(request, reply) {
   return { hello: "world" };
 });
- */
 
-/* 
 server.post("/login", async function handler(request, replay) {
   console.log(request.body);
   return { route: "login" };
 });
- */
 
 // Run the server!
 try {
