4682a6c1ae2ffdffde111f7d20d1e5f4defe0f64
disappearingnow <disappearing.now@gmail.com>
2024-10-23 16:26:45 -0400

    return products from mock api

--- server/src/routes/productsRoute.ts
+++ server/src/routes/productsRoute.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance } from "fastify";
+import { fetchProducts } from "../api/_index.js";
 
-export async function productsRoutes(fastify: FastifyInstance, options: object) {
+export async function productsRoutes(
+  fastify: FastifyInstance,
+  options: object
+) {
   fastify.get("/products", async (request, reply) => {
-    return { route: "products" };
+    const products = await fetchProducts();
+    return { products };
   });
 }
